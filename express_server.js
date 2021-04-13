4532526a15fac8afaec8781608f25f9fb416df2d
ArvinRad <radanarvin@gmail.com>
2021-04-13 08:16:47 +0000

    Add to the URL database and render the url list

--- express_server.js
+++ express_server.js
@@ -50,8 +50,9 @@ app.get("/urls/:shortURL", (req, res) => {
 
 //Adding a Post handler:
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  urlDatabase[generateRandomString()] = req.body["longURL"];  // Log the POST request body to the console
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);         // Respond with 'Ok' (we will replace this)
 });
 
 //Creating a HTML page:
